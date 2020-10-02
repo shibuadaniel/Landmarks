eb19e80a3ef5cae3fc1a3f9305e4f264aba80935
shibudaniel <47894492+shibuadaniel@users.noreply.github.com>
2020-10-02 15:25:48 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by SHIBU DANIEL on 10/2/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
