e0bf5183c5e688e3024973747e2351e3fd1faa25
shibudaniel <47894492+shibuadaniel@users.noreply.github.com>
2020-10-05 14:02:04 -0400

    ListBuilder

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by SHIBU DANIEL on 10/5/20.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarkData[0])
+            LandmarkRow(landmark: landmarkData[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
