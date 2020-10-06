e097dbeb358263110d60c0e885eb39152fe90a6e
shibudaniel <47894492+shibuadaniel@users.noreply.github.com>
2020-10-06 15:24:11 -0400

    List view and detail complete

--- Landmarks/LandmarkList.swift
+++ Landmarks/LandmarkList.swift
@@ -22,7 +22,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
-            
+            ForEach(["iPhone 11 Pro"], id: \.self) { deviceName in
+                LandmarkList()
+                    .previewDevice(PreviewDevice(rawValue: deviceName))
+                    .previewDisplayName(deviceName)
+            }
     }
 }
